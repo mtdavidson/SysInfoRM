1b56c40f572f2e46b4b52d30a82aee28c28b215c
mtdavidson <mark@4each.co.uk>
2010-02-11 16:34:58 +0000

    Update now using amara for XML Parsing

--- sysinform.py
+++ sysinform.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python
+###############################################################################
+#
+# SysInfoRM - System Information Remote Monitoring
+#
+###############################################################################
+#
+# TODO: Set License
+# TODO: User assertions and other error handling.
+# TODO: Introduce methods and classes.
 
 import urllib2
-import libxml2
+import time
 
-from elementtree.ElementTree import parse, XML, fromstring, tostring
+import amara
+from amara import bindery
 
-file = open('config.xml', "r");
-tree = parse(file);
+configXML = bindery.parse('config.xml');
+config = configXML.SysInfoRM;
 
-for host in tree.findall("//Hosts/host"):
-  print host.get('group');
+for host in config.Hosts.host:
+	print "Importing %s XML" % host.name;
+	print host.sysinfourl;
+	# TODO: Validate that URL is Valid
+	sysInfoXML = urllib2.urlopen(str(host.sysinfourl)); # TODO: Need to expand on this to make valid for mod_security
+	# TODO: Validate that reutrned data is both XML and valid.
+	print len(sysInfoXML.read());	
+	time.sleep (20);
