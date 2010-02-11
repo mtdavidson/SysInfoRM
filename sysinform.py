c4bb9b87ee2794f92c719d58795a81d01ccd8f1c
mtdavidson <mark@4each.co.uk>
2010-02-11 22:30:58 +0000

    XML structure basic checking introduced

--- sysinform.py
+++ sysinform.py
@@ -15,6 +15,23 @@ import time
 import amara
 from amara import bindery
 
+from xml.parsers.xmlproc import xmlproc
+from xml.parsers.xmlproc import xmlval
+from xml.parsers.xmlproc import xmldtd
+
+from pprint import pprint
+
+# code to handle XML parsing goes here
+class MyApp(xmlproc.Application):
+  def handle_start_tag(self,name,attrs):
+    pass
+  def handle_end_tag(self,name):
+    pass
+  def handle_data(self,data,start,end):
+    pass
+  def handle_comment(self,data):
+    pass
+
 configXML = bindery.parse('config.xml');
 config = configXML.SysInfoRM;
 
@@ -23,9 +40,20 @@ while (1):
 		print "Importing %s XML" % host.name;
 		print host.sysinfourl;
 		# TODO: Validate that URL is Valid
-		req = urllib2.Request(str(host.sysinfourl), {}, {'User-Agent' : str(config.Config.UserAgent), 'Accept' : 'text/xml'});
-		sysInfoXML = urllib2.urlopen(req);
-		# TODO: Validate that reutrned data is both XML and valid.
-		print len(sysInfoXML.read());	
-	
-	time.sleep (20); # This is just for testing final version will monitor timing in another thread
+		req = urllib2.Request(
+			str(host.sysinfourl), 
+			{}, 
+			{'User-Agent' : str(config.Config.UserAgent), 'Accept' : 'text/xml'}
+		);
+		sysInfoXML = urllib2.urlopen(req).read();
+		p = xmlproc.XMLProcessor();
+		p.set_application(MyApp());
+		if (str(p.parse_string(sysInfoXML))  == 'None'):
+			# TODO: Validate that reutrned data is both XML and valid.
+			sysInfo = bindery.parse(sysInfoXML);
+			print sysInfo.phpsysinfo.Vitals.IPAddr;
+			# Now match against rules and checks in config XML
+		else:
+			sys.stderr.write('Error XML structure invalid');
+
+	time.sleep (30); # This is just for testing final version will monitor timing in another thread
